b84a8fe9f726276b3b4f72d135fec01cceb8d533
Egbewatt Kokou <ksupro1@gmail.com>
2020-07-25 14:41:42 +0100

    Added: _3rdParty config

---
+++ _3rdParty/Core/config/config.php
@@ -0,0 +1,54 @@
+<?php
+
+    require 'vendor/autoload.php';
+    use Dotenv\Dotenv;
+
+    $dotenv = Dotenv::createImmutable("../../.env");
+    $dotenv->load();
+
+    echo $_ENV['LEADWAY_AUTH_KEY'];
+    echo $_ENV['DB_HOST'];
+    echo $_ENV['DB_PORT'];
+    echo $_ENV['DB_DATABASE'];
+    echo $_ENV['DB_USERNAME'];
+    echo $_ENV['DB_PASSWORD'];
+
+
+    class config
+    {
+        private static string $LEADWAYAUTH_KEY;
+        private static string $DATABASE_URI;
+        private static string $DATABASE_USER;
+        private static string $DATABASE_PASS;
+        
+        public function __construct()
+        {
+            // leadway api auth
+            config::$LEADWAYAUTH_KEY = $_ENV[''];
+            // build DB_URI
+            // "mysql:host=$host;port=$port;charset=utf8mb4;dbname=$db",
+            
+
+        }
+
+        public function _get_LEADWAYAUTH()
+        {
+            return config::$LEADWAYAUTH_KEY;
+        }
+
+        public function _get_DATABASE_URI()
+        {
+            return config::$DATABASE_URI;
+        }
+        public function _get_DATABASE_USER()
+        {
+            return config::$DATABASE_USER;
+        }
+        public function _get_DATABASE_PASS()
+        {
+            return config::$DATABASE_PASS;
+        }
+
+    }
+
+?>
---
+++ _3rdParty/Core/utils/ConfigException.php
@@ -0,0 +1,17 @@
+<?php
+
+    require 'vendor/autoload.php'; 
+    use PHPMailer\PHPMailer\Exception;
+
+    class ConfigException extends Exception {
+
+        public function errorMessage() {
+            //error message
+            $errorMsg = 'Error on line '.$this->getLine().' in '.$this->getFile()
+            .': <b>'.$this->getMessage().'</b> NULL / Invalid ENVIROMENT Config variable';
+            return $errorMsg;
+        }
+
+    }
+
+?>
