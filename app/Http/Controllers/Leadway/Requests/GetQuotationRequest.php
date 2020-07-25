4e3f8a118deae0d30af0e8f6cfe5b80cb0ad9b2a
Egbewatt Kokou <ksupro1@gmail.com>
2020-07-25 15:27:22 +0100

    API requests

---
+++ app/Http/Controllers/Leadway/Requests/GetQuotationRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Leadway\Requests;
+
+use OpenApi\Annotations\Items;
+use OpenApi\Annotations\Property;
+use OpenApi\Annotations\Schema;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+/**
+ * @Schema(
+ *      description= "Leadway Health Insurance request"
+ * )
+ *
+ * @package App\Http\Controllers\Leadway\Requests
+ */
+class GetQuotationRequest extends Controller
+{
+
+    public function __construct(Request $request)
+   {
+      $this->validate(
+         $request, [
+            'signature' => 'string',
+            'staffs' => 'string',
+            'building' => 'string',
+            'vehicle' => 'required|unique:Vehicle',
+            'clientNo' => 'string',
+            'clientInfo' => 'unique:ClientInfo',
+            'productCode' => 'string',
+            'productSubClass' => 'string'
+         ]
+      );
+
+      parent::__construct($request);
+   }
+
+}
+
+?>
