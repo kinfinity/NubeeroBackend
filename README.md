5d967a08aeb87a997abb8c0198eaeb9bdce820f7
Egbewatt Kokou <ksupro1@gmail.com>
2020-07-27 18:55:05 +0100

    -- base

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # NubeeroBackend
+
+    php -S localhost:8000 -t public/index.php
+
+    php artisan swagger-lume:publish
+
+    php artisan swagger-lume:generate
+
+    http://localhost:8000/api/documentation
---
+++ tests/ResetAuthTest.php
@@ -0,0 +1,17 @@
+<?php
+
+    use PHPUnit\Framework\TestCase;
+
+    class ResetAuthTest extends TestCase
+    {
+        /**
+         */
+        public function testResetAuth_Leadway()
+        {
+            $this->assertTrue(true);
+            $this->assertFalse(false);
+            //$this->assertSame());
+        }
+    }
+
+?>
