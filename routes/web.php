73e2c0130b3b480c00ff41876b47e30b32ef9149
Egbewatt Kokou <ksupro1@gmail.com>
2020-07-30 08:12:52 +0100

    comments --fix

--- routes/web.php
+++ routes/web.php
@@ -10,29 +10,22 @@
 // AUTHENTICATION $ AUTHORIZATION
 $router->group(['prefix' => 'api'], function () use ($router) {
     
+    // Matches /api/*
     $router->get('/', function () use ($router) {
         return "Welcome to Nubeero APIs";
         // return $router->app->version();
     });
     
-    // Matches "/api/register
     $router->post('/register', 'AuthController@register');
-    // Matches "/api/login
     $router->post('login', 'AuthController@login');
-
-    // Matches "/api/profile
     $router->get('profile', 'UserController@profile');
 
-    // Matches "/api/users/1 
-    //get one user by id
+    $router->get('users', 'UserController@allUsers');
+    // Matches "/api/users/* 
     $router->get('users/{id}', 'UserController@singleUser');
 
-    // Matches "/api/users
-    $router->get('users', 'UserController@allUsers');
-
-
     // INSURANCE
-    $router->post('/HealthInsurance', 'HealthInsuranceController@post');
+    $router->post('/healthinsurance', 'HealthinsuranceController@post');
     $router->post('/MotorInsurance', 'AutobaseMotorInsuranceController@post');
     $router->post('/GetQuotation', 'GetQuotationController@post');
     $router->post('/CreatePolicyfromQuotation', 'CreatePolicy4mQuotationController@post');
